23e2d756f4262325205e60f16f35191bd745b00b
Samuel <samuel.schuetz6@gmail.com>
2025-09-04 20:44:39 -0600

    Still need to do pt. 2 and understand lambda

--- Racket/hw2.rkt
+++ Racket/hw2.rkt
@@ -1,5 +1,8 @@
 #lang racket 
 
+
+
+
 (define (lookup symbl env)
         (cond
            [(null? env) (error "no environment to look in, or enviroment not right")]
@@ -10,10 +13,12 @@
 
 ; (define sample-env '((a . apple)(b . bear)(c . can)(d . dove)))
 (define _enviroment
-    '( (+ . ,+) ;arithmetic
-       (- . ,-) ; , means take the literal function, sorta
+    `( (+ . ,+) ;arithmetic       ` =quasiquote
+       (- . ,-) ; , =unquote operator
        (* . ,*)
        (/ . ,/)
+       (= . ,=)
+       (eq? . ,eq?)
     )
 )
 
@@ -22,10 +27,61 @@
 (define  (sam-eval exprn env)
     (cond 
     [(number? exprn) exprn]
-    [(symbol? exprn) 
-        (lookup exprn env)
-    ]
+    [(symbol? exprn) (lookup exprn env)]
+    [(list? exprn) (cond
+                    [(eq? (car exprn) 'quote) (cadr exprn)]
+                    [(eq? (car exprn) 'if) (if  ;(if test then-expr else-expr)
+                            (sam-eval (cadr exprn) env)
+                            (sam-eval (caddr exprn) env)
+                            (sam-eval (cadddr exprn) env) )
+                    ]
+                    [(eq? (car exprn) 'lambda) ;also chatgpt
+                            (λ (arg)
+                         (sam-eval (caddr exprn) 
+                            (cons (cons (car (cadr exprn)) arg) env)))] 
+                    [else
+                        (apply (sam-eval (car exprn) env)         ; evaluate function, this part is all chatgpt did not have time to understand
+                        (map (λ (e) (sam-eval e env))      ; evaluate args
+                        (cdr exprn))
+                    )]
+    )]
+    
     [else (error "Don't know what to do next")])
 )
 
-(sam-eval (+ 1 2) _enviroment)
+ 
+
+
+ ;;test cases for proff
+(sam-eval '(quote hello) _enviroment)
+(sam-eval '(quote (1 2 3)) _enviroment)
+
+(sam-eval '(if (= 2 2) 100 200) _enviroment)
+(sam-eval '(if (= 3 4) 7 8) _enviroment)
+
+(sam-eval '(+ 10 5) _enviroment)
+(sam-eval '(* 3 7) _enviroment)
+
+(sam-eval '((lambda (x) (+ x 2)) 4) _enviroment)
+(sam-eval '((lambda (y) (* y y)) 6) _enviroment)
+ ;lambda test cases
+
+;(sam-eval '(lambda (x) (+ x 1)) _enviroment)
+;; => #<procedure>
+
+;(sam-eval '((lambda (x) (+ x 1)) 5) _enviroment)
+;; => 6
+
+;(sam-eval '((lambda (y) (* y y)) 4) _enviroment)
+;; => 16
+
+
+
+
+
+
+
+
+
+
+
